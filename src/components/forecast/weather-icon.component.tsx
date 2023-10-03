052153fcd277d817a312860d7d8ab906d269a32e
Raul Gonçalves <raulmgoncalves21@gmail.com>
2023-10-03 02:49:35 +0100

    Alteração geral nos ícones

--- src/components/forecast/weather-icon.component.tsx
+++ src/components/forecast/weather-icon.component.tsx
@@ -1,8 +1,14 @@
 const WeatherIcons = {
-  CLOUDS: "/weather-icons/clouds.png",
-  RAIN: "/weather-icons/rain.png",
-  DAY_CLEAR: "/weather-icons/clear.png",
-  NIGHT_CLEAR: "/weather-icons/night.png",
+  DAY_CLEAR: "/weather-icons/clear_day.png",
+  DAY_CLOUDS: "/weather-icons/cloudy_day.png",
+  DAY_RAIN: "/weather-icons/rainy_day.png",
+  DAY_STORM: "/weather-icons/stormy_day.png",
+  DAY_SNOW: "/weather-icons/snowy_day.png",
+  NIGHT_CLEAR: "/weather-icons/clear_night.png",
+  NIGHT_CLOUDS: "/weather-icons/cloudy_night.png",
+  NIGHT_RAIN: "/weather-icons/rainy_night.png",
+  NIGHT_STORM: "/weather-icons/stormy_night.png",
+  NIGHT_SNOW: "/weather-icons/snowy_night.png",
 };
 
 type WeatherIconProps = {
@@ -24,10 +30,32 @@ function WeatherIconComponent(props: WeatherIconProps) {
       }
       break;
     case "Clouds":
-      imageSrc = WeatherIcons.CLOUDS;
+      if (hour >= 20 || hour <= 7) {
+        imageSrc = WeatherIcons.NIGHT_CLOUDS;
+      } else {
+        imageSrc = WeatherIcons.DAY_CLOUDS;
+      }
       break;
     case "Rain":
-      imageSrc = WeatherIcons.RAIN;
+      if (hour >= 20 || hour <= 7) {
+        imageSrc = WeatherIcons.NIGHT_RAIN;
+      } else {
+        imageSrc = WeatherIcons.DAY_RAIN;
+      }
+      break;
+    case "Snow":
+      if (hour >= 20 || hour <= 7) {
+        imageSrc = WeatherIcons.NIGHT_SNOW;
+      } else {
+        imageSrc = WeatherIcons.DAY_SNOW;
+      }
+      break;
+    case "Storm":
+      if (hour >= 20 || hour <= 7) {
+        imageSrc = WeatherIcons.NIGHT_STORM;
+      } else {
+        imageSrc = WeatherIcons.DAY_STORM;
+      }
       break;
     default:
       break;
